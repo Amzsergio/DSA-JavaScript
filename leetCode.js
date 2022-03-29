0b68d1458df09fc25c8921bcbf10be7a40b91824
Amzsergio <ac.sergioandres@gmail.com>
2022-03-29 11:06:15 -0500

    Exercise #1

---
+++ leetCode.js
@@ -0,0 +1,47 @@
+/*
+ #1 Two Sum 
+A. 3/29/22 My solution only works for some arrays, I've not found the logic problem. 
+B. 3/29/22 Optimal solution here: https://www.youtube.com/watch?v=IufUNRCQ37E&list=PLY5pAT_51eGyo4ixvdZgzY57N0_R1QMtb&ab_channel=ThinkFWD 
+*/
+
+//let nums = [2, 2, 5, 3, 4, 4] ** 6
+//target = 8
+// let newArr = [4,]; 1
+// let acc = 4
+// i = 1
+//lookFor = t - acc = 9
+// j = 5
+//k = 5
+
+// let nums = [2, 4, 3, 4] 
+// let target = 8
+// var twoSum = function(nums, target) {
+//   let newArr = [];
+//   let acc = nums[0];
+//   for (let i=0; newArr.length < 2; i++){
+//     let lookFor = target - acc;
+//     for(let j=0; j<nums.length; j++){
+//       if(lookFor === nums[j] && newArr.length < 2){
+//         newArr.push(j)
+//         for(let k=0; k<nums.length; k++){
+//           if(acc === nums[k] && k !== j){
+//             newArr.push(k)
+//           }
+//         }
+//       }
+//     }
+//     if (acc <= target){
+//       acc += nums[i+1];
+//     }else if(acc > target){
+//       acc = acc - target;
+//         }
+//     }
+//     return newArr;
+// };
+
+/*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+
+/*
+
+
+ */
