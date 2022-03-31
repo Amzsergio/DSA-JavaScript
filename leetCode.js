a737b974f4dae8a7d47ac32da2924eb9d13f592d
Amzsergio <ac.sergioandres@gmail.com>
2022-03-30 20:15:34 -0500

    Create Phone Number

--- leetCode.js
+++ leetCode.js
@@ -42,6 +42,41 @@ B. 3/29/22 Optimal solution here: https://www.youtube.com/watch?v=IufUNRCQ37E&li
 /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
 
 /*
-
+#200 Number of Islands
+https://www.youtube.com/watch?v=r260LwU67Uo&list=PLY5pAT_51eGyo4ixvdZgzY57N0_R1QMtb&index=2&ab_channel=ThinkFWD
+A. study how to go over a 2d grid. 
 
  */
+
+
+// CODEWARS
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+// Example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// The returned format must be correct in order to complete this challenge.
+// Don't forget the space after the closing parentheses!
+
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+function createPhoneNumber(numbers){
+    let newArr = []
+    if(numbers.length < 5) return `-${numbers.join('')}`
+    else if(numbers.length < 10 && numbers.length > 4){
+        for(let i=0; i<3; i++){
+            let aux = numbers.shift();
+            newArr.push(aux);
+        }
+        return newArr.join('') + createPhoneNumber(numbers);
+    }else if(numbers.length === 10){
+        let newArr = []
+        for(let i=0; i<3; i++){
+            let aux = numbers.shift();
+            newArr.push(aux);
+        }
+        return `(${newArr.join('')}) ${createPhoneNumber(numbers)}`
+    }
+  }
+
+  createPhoneNumber(numbers)
