6d0343ed598411d1cdc497dd21d9c74fa8994b84
Amzsergio <ac.sergioandres@gmail.com>
2022-05-13 17:10:34 -0500

    CODEWARS Multiples of 3 or 5

--- JSChallenge.js
+++ JSChallenge.js
@@ -737,8 +737,35 @@ function countDeep(arr) {
 }
 
 
+////////////////////////////////////////////////////////////////////////////
 
+//CODEWARS Multiples of 3 or 5 
+/* 
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+*/
+
+function solution(number){
+  
+  if(number > 0){
+    let mult = new Set();
+    for( let i = 0; i < number; i++ ){
+      if( (i%3)===0 || (i%5)===0 ){
+        mult.add(i)
+      }
+    } 
+  
+      return Array.from(mult).reduce((acc, ele) => {
+        return acc + ele
+      }, 0)
+  }
+   
+  return 0
+  
+}
 
 
 
